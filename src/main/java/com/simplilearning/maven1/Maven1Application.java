c2e5e0c03b850688fb4e28c55a22d9a7fbe55e98
mallela009 <mallelaarjuna009@gmail.com>
2022-11-30 17:34:16 +0530

    first commit

---
+++ src/main/java/com/simplilearning/maven1/Maven1Application.java
@@ -0,0 +1,13 @@
+package com.simplilearning.maven1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Maven1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearning/maven1/Maven1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearning.maven1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maven1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
